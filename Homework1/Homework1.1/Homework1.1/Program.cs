2bd12b915888a18063b38a761a9701e22621802c
abchrisxxx <67895459+abchrisxxx@users.noreply.github.com>
2021-03-02 16:29:16 +0800

    Add files via upload

---
+++ Homework1/Homework1.1/Homework1.1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1._1
+{
+    class Program
+    {
+        private static bool isNum(string s)
+        {
+            byte temp;
+            for (int i = 0; i < s.Length; i++)
+            {
+                temp = Convert.ToByte(s[i]);
+                if (temp < 48 || temp > 57)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string s = "";
+            double x = 0;
+            double y = 0;
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Write("Please input a number x:");
+                s = Console.ReadLine();
+                while (!isNum(s))
+                {
+                    Console.WriteLine("Invalid input!Try again.");
+                    s = Console.ReadLine();
+                }
+                x = Double.Parse(s);
+                Console.Write("Please input a number y:");
+                s = Console.ReadLine();
+                while (!isNum(s))
+                {
+                    Console.WriteLine("Invalid input!Try again.");
+                    s = Console.ReadLine();
+                }
+                y = Double.Parse(s);
+                Console.Write("Please input an operator(+ - * /):");
+                bool flag = true;
+                while (flag)
+                {
+                    s = Console.ReadLine();
+                    flag = false;
+                    switch (s)
+                    {
+                        case "+":
+                            Console.WriteLine($"x + y = {x + y}");
+                            break;
+                        case "-":
+                            Console.WriteLine($"x - y = {x - y}");
+                            break;
+                        case "*":
+                            Console.WriteLine($"x * y = {x * y}");
+                            break;
+                        case "/":
+                            if (y == 0)
+                            {
+                                Console.WriteLine("A number can not be divided by 0!");
+                            }
+                            else
+                                Console.WriteLine($"x / y = {x / y}");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input!Try again!");
+                            flag = true;
+                            break;
+
+                    }
+                }
+                Console.WriteLine("To end the program, input 0. Otherwise, the program will continue.");
+                s = Console.ReadLine();
+                if (s != "" && isNum(s))
+                {
+                    if (int.Parse(s) == 0)
+                    {
+                        exit = true;
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Homework1/Homework1.2/Homework1.2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static bool isNum(string s)
+        {
+            if (s == "")
+            {
+                return false;
+            }
+            byte temp;
+            for (int i = 0; i < s.Length; i++)
+            {
+                temp = Convert.ToByte(s[i]);
+                if (temp < 48 || temp > 57)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (isNum(textBox1.Text) && isNum(textBox2.Text) && comboBox1.Text != null)
+            {
+                if (comboBox1.Text == "/" && Double.Parse(textBox2.Text) == 0)
+                {
+                    label1.Text = "Invalid input!A number cannot be divided by 0!";
+                }
+                else
+                {
+                    double x = Double.Parse(textBox1.Text);
+                    double y = Double.Parse(textBox2.Text);
+                    string s = comboBox1.Text;
+                    switch (s)
+                    {
+                        case "+":
+                            label1.Text=($"{x} + {y} = {x + y}");
+                            break;
+                        case "-":
+                            label1.Text = ($"{x} - {y} = {x - y}");
+                            break;
+                        case "*":
+                            label1.Text = ($"{x} * {y} = {x * y}");
+                            break;
+                        case "/":
+                            label1.Text = ($"{x} / {y} = {x / y}");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                label1.Text = "Invalid input!Check your input and try again.";
+            }
+            
+        }
+    }
+}
